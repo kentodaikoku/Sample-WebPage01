fc6cb3a5f338b65757d1df14343ded685d3c7ce4
kentodaikoku <kedks090597@gmail.com>
2022-09-16 17:09:38 +0900

    レイアウトの修正、商品一覧ページ作成

--- main.js
+++ main.js
@@ -11,3 +11,30 @@ $(function() {
     $('header').removeClass('open');
   });
 });
+
+function showNext() {
+  let html = '<li>' + '<a href="item.html?item=13">' +
+                '<img src="img/item13.jpeg" alt="">' +
+                '<p>プロダクトタイトル</p>' +
+                '<p>&yen;99,999 +tax</p>' + '</a>' + 
+              '</li>' +
+              '<li>' + '<a href="item.html?item=14">' +
+                '<img src="img/item14.jpeg" alt="">' +
+                '<p>プロダクトタイトル</p>' +
+                '<p>&yen;99,999 +tax</p>' + '</a>' + 
+              '</li>' +
+              '<li>' + '<a href="item.html?item=15">' +
+                '<img src="img/item15.jpeg" alt="">' +
+                '<p>プロダクトタイトル</p>' +
+                '<p>&yen;99,999 +tax</p>' + '</a>' + 
+              '</li>' +
+              '<li>' + '<a href="item.html?item=16">' +
+                '<img src="img/item16.jpeg" alt="">' +
+                '<p>プロダクトタイトル</p>' +
+                '<p>&yen;99,999 +tax</p>' + '</a>' + 
+              '</li>';
+
+  $('.product-list').html(html);
+
+}
+
